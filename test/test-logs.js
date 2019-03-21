6c5aa1e7fb94f244c99ea94f03d53ea1f2a156de
Matthew Crocker <matthewcroc@gmail.com>
2019-03-21 08:08:40 -0500

    Additional config options. Truncuated log output.

--- test/test-logs.js
+++ test/test-logs.js
@@ -3,47 +3,46 @@ const udu = require('../index');
 const logger = udu.createUduLogger({
   level: 'log',
   timeFormat: 'MM/DD/YYYY kk:mm:ss',
+  logLimit: 50,
+  logLimitLevel: 'warning',
   metadata: {
     user: 'Matthew'
   },
   transports: [
     new udu.Transports.Console({}),
-    /* new udu.Transports.Elastic({
+    new udu.Transports.Elastic({
       host: '127.0.0.1:9200',
       index: 'engineers',
       type: '_doc',
-    }) */
+    })
   ]
 });
 
 /*
 console.log('Starting logging');
 console.log('This machine\'s hostname:', require('os').hostname());
-*/
-console.log('Error test:', new Error('testing'));
-// console.log(Array);
 
-// console.log(require('fs'));
-// console.log(JSON.stringify(require('fs'), null, 4));
-/*
+console.log('Error test:', new Error('testing'));
+console.log(Array);
+console.log(require('fs'));
+console.log(JSON.stringify(require('fs'), null, 4));
+
 console.log('This is a simple object', { a: 1, b: 2 });
 console.log('null =', null, 'and undefined =', undefined, '\n\tand true =', true, 'and NaN =', NaN);
 console.log('port:', 80);
-
+*/
 logger.log('Starting logging');
 logger.log('This machine\'s hostname:', require('os').hostname());
-*/
+
 logger.log('Error test:', new Error('testing'));
+logger.log(Array);
+logger.log(require('fs'));
+logger.log(JSON.stringify(require('fs'), null, 4));
 
-// logger.log(Array);
-// logger.log(require('fs'));
-
-// logger.log(JSON.stringify(require('fs'), null, 4));
-/*
-logger.log('This is a simple object', { a: 1, b: 2 });
-logger.log('null =', null, 'and undefined =', undefined, '\n\tand true =', true, 'and NaN =', NaN);
-logger.log('port:', 80);
-*/
+logger.info('This is a simple object', { a: 1, b: 2 });
+logger.warn('null =', null, 'and undefined =', undefined, '\n\tand true =', true, 'and NaN =', NaN);
+logger.error('port:', 80);
+logger.log('abcdefghijklmnopqrstuvwxyz');
 
 // const str = 'AAA'.repeat(30000);
 
